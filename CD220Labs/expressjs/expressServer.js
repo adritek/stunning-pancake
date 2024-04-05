d90f265a6a4b67a974cb66bbe27995ac58b2c8d7
Adrian Rogers <adritek@gmail.com>
2024-04-05 09:46:08 -0400

    add route fetchmonth

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = new express();
 
 let loginDetails = [];
+const months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+];
 
 app.get("/",(req,res)=>{
     res.send("Welcome to the express server")
@@ -19,6 +24,16 @@ app.post("/login/:name",(req,res)=>{
 app.get("/:name",(req,res)=>{
     res.send("Hello "+req.params.name)
 })
+
+// fix
+app.get("/fetchMonth/:num", (req, res) => {
+    let index = parseInt(num) - 1;
+    if (index >= 1 && index < months.length) {
+        res.send(months[index]);
+    } else {
+        res.status(400).send("Invalid month number");
+    }
+});
 
 app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
