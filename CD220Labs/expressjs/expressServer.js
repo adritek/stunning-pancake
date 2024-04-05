cf1edb03ceb78f0fd3d068200e3ff72468794455
Adrian Rogers <adritek@gmail.com>
2024-04-05 09:48:55 -0400

    formatting

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -8,21 +8,21 @@ const months = [
     "September", "October", "November", "December"
 ];
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Welcome to the express server")
 })
 
-app.get("/loginDetails",(req,res)=>{
+app.get("/loginDetails", (req, res) => {
     res.send(JSON.stringify(loginDetails));
 })
 
-app.post("/login/:name",(req,res)=>{
+app.post("/login/:name", (req, res) => {
     loginDetails.push({"name":req.params.name,"login_time":new Date()});
-    res.send(req.params.name + ", You are logged in!")
+    res.send(`${req.params.name}, You are logged in!`)
 })
 
-app.get("/:name",(req,res)=>{
-    res.send("Hello "+req.params.name)
+app.get("/:name", (req, res) => {
+    res.send(`Hello ${req.params.name}`)
 })
 
 // fix
